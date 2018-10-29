75c997d374aeb3bd9ea3bd21782097d6916b4ea5
v_zhangxiaoqing01 <v_zhangxiaoqing01@baidu.com>
2018-10-29 17:18:55 +0800

    Initial Commit

---
+++ IDLFaceDetectSDKOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IDLFaceDetectSDKOC
+//
+//  Created by v_zhangxiaoqing01 on 2018/10/29.
+//  Copyright © 2018年 zhangxiaoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
