32b118c753b226de4bec342d28afe52984b715be
v_zhangxiaoqing01 <v_zhangxiaoqing01@baidu.com>
2018-11-22 10:44:07 +0800

    commit

---
+++ FaceSDK-Collect-Basic-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IDLFaceDetectSDKOC
+//
+//  Created by v_zhangxiaoqing01 on 2018/10/29.
+//  Copyright © 2018年 zhangxiaoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceSDK-Collect-Basic-iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IDLFaceDetectSDKOC
+//
+//  Created by v_zhangxiaoqing01 on 2018/10/29.
+//  Copyright © 2018年 zhangxiaoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
